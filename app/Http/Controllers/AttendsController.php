160639a8c794170efe65eb5a4577e34f7b9c040a
InJoon-L <dlswns619@naver.com>
2021-08-08 22:08:25 +0900

    attends api 수정

--- app/Http/Controllers/AttendsController.php
+++ app/Http/Controllers/AttendsController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Attend;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AttendsController extends Controller
 {
-    public function attend() {
+    public function attend(Request $req) {
         $date = date("Y-m-d H:i:s");
         $time = date("H:i:s");
         $point = strtotime('09:00:00');
@@ -17,7 +18,8 @@ class AttendsController extends Controller
         $result = $timestamp - $point;
 
         $attend = new Attend();
-        $userId = Auth::user()->id;
+        // $userId = Auth::user()->id; // api.php는 비저장 상태라 토큰을 받아오지 않는 한 인증이 불가
+        $userId = User::where('sid', $req-> sid)->first()->id;
 
         if ($result > 0) {
             $cul = number_format($result/60/5); // 바퀴 수
