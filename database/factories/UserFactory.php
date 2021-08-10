3d42ddbb1d241e7ab78f2b4bc258098f19586d52
lizill <lizill@naver.com>
2021-08-10 13:59:34 +0900

    User Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,11 +25,18 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
+            // 'name' => $this->faker->name(),
+            // 'email' => $this->faker->unique()->safeEmail(),
+            // 'email_verified_at' => now(),
+            // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            // 'remember_token' => Str::random(10),
+
             'name' => $this->faker->name(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'provider' => 'kakao',
             'remember_token' => Str::random(10),
+            'current_team_id' => '4' // 1, 2, 3, 4
         ];
     }
 
