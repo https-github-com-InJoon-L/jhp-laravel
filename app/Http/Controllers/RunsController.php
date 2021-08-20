f7067dab3ede855ef3f0d69c594c57b26fa41063
InJoon-L <dlswns619@naver.com>
2021-08-20 15:11:59 +0900

    달리기 인증 게시판 승인 권한 및 승인 처리된 게시글 핸들링

--- app/Http/Controllers/RunsController.php
+++ app/Http/Controllers/RunsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Attend_posts;
 use App\Models\Run;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -12,22 +13,43 @@ class RunsController extends Controller
     // user_id랑 달린 수(run)
     public function minusRun(Request $req, $selected_post_id) {
         $validator = Validator::make($req->all(), [
-            'user_id' => 'required|integer',
-            'run' => 'required|integer',
+            'user_id' => 'required|integer', // 달리기 게시판 글쓴이 아이디
+            'req_id' => 'required|integer', // 승인 버튼 누른 사람 아이디
+            'run' => 'required|integer', // 달리기 수
         ]);
 
         if($validator->fails()){
             return response()->json($validator->errors()->toJson(), 400);
         }
 
+        $user = User::find($req->req_id);
+
+        if (!($user->current_team_id == 4 || $user->current_team_id == 5)) {
+            $res = response()->json([
+                'status' => 'false',
+                'message' => '권한이 없습니다.',
+            ], 403);
+
+            return $res;
+        }
+
         $run = Run::where('user_id', $req->user_id)->first();
+        $post = Attend_posts::find($selected_post_id);
+
+        if ($post->flag == true) {
+            $res = response()->json([
+                'status' => 'false',
+                'message' => '이미 승인처리가 된 게시글입니다.',
+            ], 403);
+
+            return $res;
+        }
 
         $run->countRun = $run->countRun - $req->run;
         $run->minusRun = $run->minusRun + $req->run;
 
         $run->save();
 
-        $post = Attend_posts::find($selected_post_id);
         $post->flag = true;
 
         $post-> save();
@@ -36,7 +58,7 @@ class RunsController extends Controller
             'status' => 'success',
             'run' => $run,
             'post' => $post->flag
-        ]);
+        ], 200);
 
         return $res;
     }
